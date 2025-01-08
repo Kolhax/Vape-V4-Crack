55305524e01cf2cdc4450409f84fde2778432f4a
Kolhax <92904192+Kolhax@users.noreply.github.com>
2025-01-08 16:14:41 -0500

    Update Launcher.py

--- Launcher.py
+++ Launcher.py
@@ -120,5 +120,6 @@ def main():
 
     logging.info("Setup and execution complete.")
 
+    logging.infor("Please Concideer leaving a star on my repo :D at: https://github.com/Kolhax/Vape-V4-Source")
 if __name__ == "__main__":
     main()
